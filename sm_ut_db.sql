d3d23f5d5281102e952221286b6a873e5da26b15
Moussa Fofana <didegassama@gmail.com>
2020-11-27 08:41:29 +0000

    import db

--- sm_ut_db.sql
+++ sm_ut_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 09 nov. 2020 à 16:15
+-- Généré le :  ven. 27 nov. 2020 à 08:40
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
