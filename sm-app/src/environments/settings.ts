6bfc853eb4814e6769ed4c27bec376a469faeb81
bamboguirassy <didegassama@gmail.com>
2021-06-18 16:53:56 +0000

    https://uidt.atlassian.net/browse/GSM-32

--- sm-app/src/environments/settings.ts
+++ sm-app/src/environments/settings.ts
@@ -2,9 +2,9 @@
 export const SETTINGS = {
   boxed: false,// boolean
   layout: 'horizontal',// horizontal | vertical
-  sidebarBg: '#3b873e',// string #bd3b19
+  sidebarBg: '#1A7D47',// string #bd3b19
   sidebarColor: '#fff',// string
   sidebarOpened: false,// boolean
-  topbarBg: '#F3F2EF',// string
+  topbarBg: '#fff',// string
   topbarColor: '#0747a6'// string
 };
